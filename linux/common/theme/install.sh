e2172a71e02f281141c80188beb33a76a3d6d00b
Destroyer <vlachmir@fit.cvut.cz>
2016-04-22 02:45:07 +0200

    Update for Xubuntu 16.04

--- linux/common/theme/install.sh
+++ linux/common/theme/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 unzip Numix\ Flat\ Dark.zip -d /usr/share/themes/
 cd /usr/share/software-center/ui/gtk3/css;
-sed -i 's/@define-color light-aubergine #DED7DB;/@define-color light-aubergine #333333;/' softwarecenter.css;
-sed -i 's/@define-color super-light-aubergine #F4F1F3;/@define-color super-light-aubergine #333333;/' softwarecenter.css
+#sed -i 's/@define-color light-aubergine #DED7DB;/@define-color light-aubergine #333333;/' softwarecenter.css;
+#sed -i 's/@define-color super-light-aubergine #F4F1F3;/@define-color super-light-aubergine #333333;/' softwarecenter.css
 #cp -f ~/_installlightdm-tk-greeter.css /usr/share/themes/Greybird/gtk-3.0/apps/
 cd /usr/share/themes/Greybird/gtk-3.0/;
 sed -i 's/@define-color lightdm_bg_color #f9f9f9;/@define-color lightdm_bg_color #242424;/' gtk.css;
