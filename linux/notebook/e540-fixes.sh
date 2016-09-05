edee1c5b1fee5ddb8860bbc102da7437917eeaa4
Destroyer <vlachmir@fit.cvut.cz>
2016-09-05 14:42:16 +0200

    Update e540-fixes.sh

--- linux/notebook/e540-fixes.sh
+++ linux/notebook/e540-fixes.sh
@@ -6,3 +6,13 @@ syndaemon -i 0.6 -K d
 # keep brightness low after startup
 echo 1057 > /sys/class/backlight/intel_backlight/brightness
 _EOF
+
+
+cat > /etc/pm/sleep.d/11_led_fix <<'_EOF'
+#!/bin/sh
+
+if [ "$1" = "resume" ]
+then
+    echo '0 on' > /proc/acpi/ibm/led
+fi
+_EOF
