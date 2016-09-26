e87f79068cba23ae12f3313aeb2e925f3cc7beb7
Destroyer <vlachmir@fit.cvut.cz>
2016-09-27 00:31:21 +0200

    Update e540-fixes.sh
    added VLC fix

--- linux/notebook/e540-fixes.sh
+++ linux/notebook/e540-fixes.sh
@@ -1,13 +1,23 @@
-cat > /etc/rc.local <<'_EOF'
-#!/bin/sh -e
 # Thinkpad E540 fixes
-# fix touchpad
-syndaemon -i 0.6 -K d
-# keep brightness low after startup
-echo 1057 > /sys/class/backlight/intel_backlight/brightness
+# Fix VLC tearing
+cat > /etc/X11/xorg.conf.d/20-intel.conf <<'_EOF'
+Section "Device"
+
+Identifier "Intel Graphics"
+Driver "intel"
+Option "AccelMethod" "sna"
+Option "TearFree" "true"
+
+EndSection
 _EOF
 
+cat > /etc/rc.local <<'_EOF'
+#!/bin/sh -e
 
+# Fix touchpad
+syndaemon -i 0.6 -K d
+
+# Fix LED after wakup
 cat > /etc/pm/sleep.d/11_led_fix <<'_EOF'
 #!/bin/sh
 
