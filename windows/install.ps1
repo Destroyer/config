6c45a27d7dc89eb438a8734e21ed4acbac9ba7fa
Destroyer <vlachmir@fit.cvut.cz>
2016-10-07 22:53:34 +0200

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,25 +1,10 @@
+# Windows 10 
 # Ask for elevated permissions if required
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
 	Exit
 }
 
-# Disable hibernation
-powercfg -h off
-
-# Disable automatic pagefile management
-$cs = gwmi Win32_ComputerSystem
-if ($cs.AutomaticManagedPagefile) {
-    $cs.AutomaticManagedPagefile = $False
-    $cs.Put()
-}
-# Disable a *single* pagefile if any
-$pg = gwmi win32_pagefilesetting
-if ($pg) {
-    $pg.Delete()
-}
- 
- 
 #Source dasm.cz
 # Disable Telemetry
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
@@ -47,11 +32,17 @@ If (!(Test-Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore"
 	New-Item -Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore" -Force | Out-Null
 }
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore" -Name "HarvestContacts" -Type DWord -Value 0
+# Disable Advertising ID
+If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo")) {
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" -Name "Enabled" -Type DWord -Value 0
 # Lower UAC level
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Type DWord -Value 0
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Type DWord -Value 0
 # Disable Windows Update automatic restart
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\WindowsStore\WindowsUpdate" -Name "AutoDownload" -Type DWord -Value 1
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\WindowsUpdate\UX\Settings" -Name "UxOption" -Type DWord -Value 1
 # Disable Lock screen
 If (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\Personalization")) {
 	New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\Personalization" | Out-Null
@@ -73,6 +64,21 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Type DWord -Value 0
 # Disable snap assist
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "SnapAssist" -Type DWord -Value 0
+# Change default Explorer view to "Computer"
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "LaunchTo" -Type DWord -Value 1
+
+# Restrict Windows Update P2P only to local network
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" -Name "DODownloadMode" -Type DWord -Value 1
+If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\DeliveryOptimization")) {
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\DeliveryOptimization" | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\DeliveryOptimization" -Name "SystemSettingsDownloadMode" -Type DWord -Value 3
+# Stop and disable Diagnostics Tracking Service
+Stop-Service "DiagTrack"
+Set-Service "DiagTrack" -StartupType Disabled
+# Stop and disable WAP Push Service
+Stop-Service "dmwappushservice"
+Set-Service "dmwappushservice" -StartupType Disabled
 
 # Uninstall default Microsoft applications
 Get-AppxPackage "Microsoft.3DBuilder" | Remove-AppxPackage
@@ -88,7 +94,11 @@ Get-AppxPackage "Microsoft.WindowsCamera" | Remove-AppxPackage
 Get-AppxPackage "Microsoft.Office.Sway" | Remove-AppxPackage
 Get-AppxPackage "9E2F88E3.Twitter" | Remove-AppxPackage
 Get-AppxPackage "king.com.CandyCrushSodaSaga" | Remove-AppxPackage
-
+# Disable OneDrive
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive")) {
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Type DWord -Value 1
 
 # Set Photo Viewer as default for bmp, gif, jpg and png
 If (!(Test-Path "HKCR:")) {
@@ -110,6 +120,22 @@ New-Item -Path "HKCR:\Applications\photoviewer.dll\shell\open\DropTarget" -Force
 Set-ItemProperty -Path "HKCR:\Applications\photoviewer.dll\shell\open" -Name "MuiVerb" -Type String -Value "@photoviewer.dll,-3043"
 Set-ItemProperty -Path "HKCR:\Applications\photoviewer.dll\shell\open\command" -Name "(Default)" -Type ExpandString -Value "%SystemRoot%\System32\rundll32.exe `"%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll`", ImageView_Fullscreen %1"
 Set-ItemProperty -Path "HKCR:\Applications\photoviewer.dll\shell\open\DropTarget" -Name "Clsid" -Type String -Value "{FFE2A43C-56B9-4bf5-9A79-CC6D4285608A}"
+
+# Disable hibernation
+powercfg -h off
+
+# Disable automatic pagefile management
+$cs = gwmi Win32_ComputerSystem
+if ($cs.AutomaticManagedPagefile) {
+    $cs.AutomaticManagedPagefile = $False
+    $cs.Put()
+}
+# Disable a *single* pagefile if any
+$pg = gwmi win32_pagefilesetting
+if ($pg) {
+    $pg.Delete()
+}
+
 
 # Restart
 Write-Host
