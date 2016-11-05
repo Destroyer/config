a16e78034f41ec2d586d6cd32918d370957b1cd6
Destroyer <vlachmir@fit.cvut.cz>
2016-11-05 03:17:15 +0100

    Update Intel_Chipset_chocolateyInstall.ps1

--- windows/Intel_Chipset_chocolateyInstall.ps1
+++ windows/Intel_Chipset_chocolateyInstall.ps1
@@ -8,11 +8,15 @@ $silentArgs = '-s -norestart'
 $unpackDir = New-Item "${ENV:TEMP}\intelchipsetdriver" -ItemType Directory -Force
 $unpackFile = "${ENV:TEMP}\Intel_Chipset_V101127.zip"
 $setupFile = Join-Path "$unpackDir" "SetupChipset.exe"
+$checksumtype = 'sha256'
+$checksum ='a27d0e2715cb6503ca645089260166f64ef42540529ad5cd8e334f994134dee3'
+$checksum64 ='a27d0e2715cb6503ca645089260166f64ef42540529ad5cd8e334f994134dee3'
 
 $url = "http://dlcdnet.asus.com/pub/ASUS/misc/utils/Intel_Chipset_V101127.zip"
-$url64 = "˜$url"
+$url64 = "$url"
 
-Get-ChocolateyWebFile $packageName $unpackFile $url $url64
+ 
+Get-ChocolateyWebFile $packageName $unpackFile $url $url64 -checksumtype $checksumtype -checksum $checksum -checksum64 $checksum
 Get-ChocolateyUnzip $unpackFile $unpackDir
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $setupFile
 Remove-Item $unpackDir -Recurse -Force
