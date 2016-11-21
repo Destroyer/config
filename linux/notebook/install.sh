1b891bfc9e13fe0e9b777e6ea6799961163faa06
Destroyer <vlachmir@fit.cvut.cz>
2016-11-21 02:05:13 +0100

    Update install.sh

--- linux/notebook/install.sh
+++ linux/notebook/install.sh
@@ -5,3 +5,6 @@ apt-get -y install obs-studio
 
 wget "http://download.teamviewer.com/download/teamviewer_i386.deb"
 dpkg -i teamviewer_i386.deb || {  apt-get -f -y install; dpkg -i teamviewer_i386.deb; }
+
+# synergy after startup
+echo -e "\n\ndisplay-setup-script=/usr/bin/synergyc IP_serveru" >> /usr/share/lightdm/lightdm.conf.d/60-lightdm-gtk-greeter.conf
