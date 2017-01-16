e20cf9982cccdae891e882cbe6b3ca48b56c1785
Destroyer <vlachmir@fit.cvut.cz>
2017-01-16 01:41:40 +0100

    Create Microsoft.PowerShell_profile.ps1

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,112 @@
+$env:Path += "C:\APPS;C:\APPS\Notepad++"
+function stream { C:\Apps\Streamlink\Streamlink.exe https://www.twitch.tv/$args source -p "C:\APPS\MPC-HC\mpc-hc64.exe" }
+function transfer { curl -Headers @{"Max-Days" = "$($args[1])"} -method put -infile .\$($args[0]) https://transfer.sh/$($args[0]) }
+ 
+
+# Linux aliases
+Set-Alias ll Get-ChildItem
+# Source: https://github.com/mikemaccana/powershell-profile/blob/master/Microsoft.PowerShell_profile.ps1
+function df {
+	get-volume
+}
+
+function sed($file, $find, $replace){
+	(Get-Content $file).replace("$find", $replace) | Set-Content $file
+}
+
+function sed-recursive($filePattern, $find, $replace) {
+	$files = ls . "$filePattern" -rec
+	foreach ($file in $files) {
+		(Get-Content $file.PSPath) |
+		Foreach-Object { $_ -replace "$find", "$replace" } |
+		Set-Content $file.PSPath
+	}
+}
+
+function grep($regex, $dir) {
+	if ( $dir ) {
+		ls $dir | select-string $regex
+		return
+	}
+	$input | select-string $regex
+}
+
+function grepv($regex) {
+	$input | ? { !$_.Contains($regex) }
+}
+
+function which($name) {
+	Get-Command $name | Select-Object -ExpandProperty Definition
+}
+function killall($name) {
+	ps $name -ErrorAction SilentlyContinue | kill
+}
+function pgrep($name) {
+	ps $name
+}
+function touch($file) {
+	"" | Out-File $file -Encoding ASCII
+}
+function sudo {
+	$file, [string]$arguments = $args;
+	$psi = new-object System.Diagnostics.ProcessStartInfo $file;
+	$psi.Arguments = $arguments;
+	$psi.Verb = "runas";
+	$psi.WorkingDirectory = get-location;
+	[System.Diagnostics.Process]::Start($psi) >> $null
+}
+
+function pstree {
+	$ProcessesById = @{}
+	foreach ($Process in (Get-WMIObject -Class Win32_Process)) {
+		$ProcessesById[$Process.ProcessId] = $Process
+	}
+
+	$ProcessesWithoutParents = @()
+	$ProcessesByParent = @{}
+	foreach ($Pair in $ProcessesById.GetEnumerator()) {
+		$Process = $Pair.Value
+
+		if (($Process.ParentProcessId -eq 0) -or !$ProcessesById.ContainsKey($Process.ParentProcessId)) {
+			$ProcessesWithoutParents += $Process
+			continue
+		}
+
+		if (!$ProcessesByParent.ContainsKey($Process.ParentProcessId)) {
+			$ProcessesByParent[$Process.ParentProcessId] = @()
+		}
+		$Siblings = $ProcessesByParent[$Process.ParentProcessId]
+		$Siblings += $Process
+		$ProcessesByParent[$Process.ParentProcessId] = $Siblings
+	}
+
+	function Show-ProcessTree([UInt32]$ProcessId, $IndentLevel) {
+		$Process = $ProcessesById[$ProcessId]
+		$Indent = " " * $IndentLevel
+		if ($Process.CommandLine) {
+			$Description = $Process.CommandLine
+		} else {
+			$Description = $Process.Caption
+		}
+
+		Write-Output ("{0,6}{1} {2}" -f $Process.ProcessId, $Indent, $Description)
+		foreach ($Child in ($ProcessesByParent[$ProcessId] | Sort-Object CreationDate)) {
+			Show-ProcessTree $Child.ProcessId ($IndentLevel + 4)
+		}
+	}
+
+	Write-Output ("{0,6} {1}" -f "PID", "Command Line")
+	Write-Output ("{0,6} {1}" -f "---", "------------")
+
+	foreach ($Process in ($ProcessesWithoutParents | Sort-Object CreationDate)) {
+		Show-ProcessTree $Process.ProcessId 0
+	}
+}
+
+function unzip ($file) {
+	$dirname = (Get-Item $file).Basename
+	echo("Extracting", $file, "to", $dirname)
+	New-Item -Force -ItemType directory -Path $dirname
+	expand-archive $file -OutputPath $dirname -ShowProgress
+}
+
