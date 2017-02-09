10ae80aab734d6a7dd540a3293e861860c895bcb
Destroyer <vlachmir@fit.cvut.cz>
2017-02-09 22:33:00 +0100

    Update install.sh
    Removed netbeans, java, sublime text
    Added intellij, VSC, strace, nethogs, iptraf, ...

--- linux/common/install.sh
+++ linux/common/install.sh
@@ -5,22 +5,19 @@ mkdir -p ~/_apps
 cd ~/_install
 
 add-apt-repository -y ppa:danielrichter2007/grub-customizer
-add-apt-repository -y ppa:webupd8team/java
 add-apt-repository -y ppa:numix/ppa
-add-apt-repository ppa:neovim-ppa/unstable
 
 apt-get update
 apt-get -y upgrade
 
 wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-wget "http://synergy-project.org/files/packages/synergy-v1.7.6-stable-bcb9da8-Linux-x86_64.deb"
-wget "https://download.sublimetext.com/sublime-text_build-3114_amd64.deb"
-wget "http://download.netbeans.org/netbeans/8.1/final/bundles/netbeans-8.1-linux.sh" -O netbeans-8.1-linux.sh
+wget "https://synergy-project.org/files/packages/synergy-v1.8.7-stable-9799e96-Linux-x86_64.deb"
+wget "https://download-cf.jetbrains.com/idea/ideaIC-2016.3.4.tar.gz" -O ideaC.tar.gz
+wget --trust-server-names "https://go.microsoft.com/fwlink/?LinkID=760868"
 wget --trust-server-names "https://www.trillian.im/get/linux/2.0/trillian_2.0.0.7_v5_amd64.deb"
 wget "http://installer.jdownloader.org/JD2Setup_x64.sh"
 
-apt-get -y install alien \
-oracle-java8-installer \
+apt -y install alien \
 grub-customizer \
 libqtgui4:amd64 libqt4-network libavahi-compat-libdnssd1 \
 git \
@@ -32,7 +29,7 @@ htop \
 numlockx \
 curl \
 vim \
-nvim \
+neovim \
 rlwrap \
 redshift \
 redshift-gtk \
@@ -40,13 +37,16 @@ numix-icon-theme \
 vlc \
 ffmpeg \
 valgrind \
-source-highlight
+source-highlight \
+unrar unace rar unrar p7zip zip unzip p7zip-full p7zip-rar file-roller \
+nethogs \
+iptraf \
+strace
 
 
  
 dpkg -i google-chrome-stable_current_amd64.deb || {  apt-get -f -y install; dpkg -i google-chrome-stable_current_amd64.deb; }
 dpkg -i synergy* || {  apt-get -f -y install; dpkg -i synergy*; }
-dpkg -i sublime-text* || {  apt-get -f -y install; dpkg -i sublime-text*; }
 dpkg -i trillian_2*  || {  apt-get -f -y install; dpkg -i trillian_2*; }
  
 # Numlock fix
@@ -55,14 +55,12 @@ echo -e "\n\ngreeter-setup-script=/usr/bin/numlockx on" >> /usr/share/lightdm/li
 echo -e "[SeatDefaults]\nallow-guest=false" >> /usr/share/lightdm/lightdm.conf.d/99-no-guest.conf
  
 # Remove obsolete apps
-apt-get -y purge gmusicbrowser pidgin simple-scan gnome-sudoku gnome-mines xfce4-dict
-apt-get clean
-apt-get autoclean
-apt-get -y autoremove
+apt -y purge gmusicbrowser pidgin simple-scan gnome-sudoku gnome-mines xfce4-dict
+apt clean
+apt autoclean
+apt -y autoremove
 
 cd ~/_install
-sh netbeans-8.1-linux.sh &
-wait
 sh JD2Setup_x64.sh &
 wait
 
