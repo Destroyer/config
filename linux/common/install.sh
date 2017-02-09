56b143e6d97cc4fea474918f003f112efdd7602d
Destroyer <vlachmir@fit.cvut.cz>
2017-02-10 00:01:05 +0100

    Update install.sh

--- linux/common/install.sh
+++ linux/common/install.sh
@@ -42,7 +42,7 @@ unrar unace rar unrar p7zip zip unzip p7zip-full p7zip-rar file-roller \
 nethogs \
 iptraf \
 strace \
-zsh \
+zsh
 
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" 
