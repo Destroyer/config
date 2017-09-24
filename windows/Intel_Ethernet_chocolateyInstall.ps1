8844bec86db9570ac5823cf390ce9626359d0f98
Destroyer <vlachmir@fit.cvut.cz>
2017-09-24 23:00:14 +0200

    Update Intel_Ethernet_chocolateyInstall.ps1

--- windows/Intel_Ethernet_chocolateyInstall.ps1
+++ windows/Intel_Ethernet_chocolateyInstall.ps1
@@ -1,23 +1,19 @@
-# Intel(R) Gigabit Ethernet Driver V20.2.4001.0 for Windows 10 64bit.(WHQL)
+# Intel(R) Gigabit Ethernet Driver
 # https://www.asus.com/Motherboards/Z170-A/HelpDesk_Download/
 
 $packageName = 'Intel_Ethernet'
-$version = '20.2.4001.0'
+$version = '22.4.16.0'
 $fileType = 'exe'
 $silentArgs = '-s -noreboot'
 $unpackDir = New-Item "${ENV:TEMP}\intelethernet" -ItemType Directory -Force
-$unpackFile = "${ENV:TEMP}\Intel_Gigabit_Ethernet_Win7-81_V20230010_Win10_V20240010.zip"
-$setupFile = Join-Path "$unpackDir" "Intel_Gigabit_Ethernet_Win7-81_V20230010_Win10_V20240010\AsusSetup.exe"
+$unpackFile = "${ENV:TEMP}\IntelLAN22.4.16.0_RS2_20170718.zip"
+$setupFile = Join-Path "$unpackDir" "AsusSetup.exe"
 $checksumtype = 'sha256'
-$checksum ='924415cc096dd26d002168162853916bbb13ffe002d018dff12450bb48e4ec8d'
-$checksum64 ='924415cc096dd26d002168162853916bbb13ffe002d018dff12450bb48e4ec8d'
-
-$url = "http://dlcdnet.asus.com/pub/ASUS/lan/Intel_Gigabit_Ethernet_Win7-81_V20230010_Win10_V20240010.zip"
-$url64 = "$url"
+$checksum64 ='9addb9212356f35415b235bd1a430a712242b1ed385ef6e785b85df1bc754ce2'
+$url64 = "http://dlcdnet.asus.com/pub/ASUS/mb/04LAN/IntelLAN22.4.16.0_RS2_20170718.zip"
 
 
- 
-Get-ChocolateyWebFile $packageName $unpackFile $url $url64 -checksumtype $checksumtype -checksum $checksum -checksum64 $checksum
+Get-ChocolateyWebFile $packageName $unpackFile -Url64bit $url64 -checksumtype $checksumtype -checksum64 $checksum
 Get-ChocolateyUnzip $unpackFile $unpackDir
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $setupFile
 Remove-Item $unpackDir -Recurse -Force
