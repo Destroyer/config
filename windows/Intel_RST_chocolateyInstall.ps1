ae90159c3777620380f31fd2c5240dca72a82b00
Destroyer <vlachmir@fit.cvut.cz>
2017-09-24 23:07:16 +0200

    Update Intel_RST_chocolateyInstall.ps1

--- windows/Intel_RST_chocolateyInstall.ps1
+++ windows/Intel_RST_chocolateyInstall.ps1
@@ -3,25 +3,20 @@
 # https://www.asus.com/Motherboards/Z170-A/HelpDesk_Download/
 
 $packageName = 'Intel_RST'
-$version = '14.8.0.1042'
+$version = '15.7.1.1015'
 $fileType = 'exe'
 $silentArgs = '-s -noreboot'
 $unpackDir = New-Item "${ENV:TEMP}\intelrst" -ItemType Directory -Force
-$unpackFile = "${ENV:TEMP}\IRST_Win7-81-10_64bit_V14801042.zip"
-$setupFile = Join-Path "$unpackDir" "\IRST_Win7-81-10_64bit_V14801042\IRST\Driver\Disk\AsusSetup.exe"
-$setupFile2 = Join-Path "$unpackDir" "\IRST_Win7-81-10_64bit_V14801042\IRST\Install\AsusSetup.exe"
+$unpackFile = "${ENV:TEMP}\IRST15.7.1.1015_RS2_20170718.zip"
+$setupFile = Join-Path "$unpackDir" "\Driver\AsusSetup.exe"
+$setupFile2 = Join-Path "$unpackDir" "\Install\AsusSetup.exe"
 $checksumtype = 'sha256'
-$checksum ='32b4b1e0134d51ef4df4c57c26d0117d17c5d06f2424c65804e7e4ee0d4a23c1'
-$checksum64 ='32b4b1e0134d51ef4df4c57c26d0117d17c5d06f2424c65804e7e4ee0d4a23c1'
-
-$url = "http://dlcdnet.asus.com/pub/ASUS/misc/sata/IRST_Win7-81-10_64bit_V14801042.zip"
-$url64 = "$url"
+$checksum64 ='cef8567c83d176096ba59d7d56013784b80e69cdef01fc89d172d06069d2aa08'
+$url64 = "http://dlcdnet.asus.com/pub/ASUS/mb/05SATA/IRST15.7.1.1015_RS2_20170718.zip"
 
 
- 
-Get-ChocolateyWebFile $packageName $unpackFile $url $url64 -checksumtype $checksumtype -checksum $checksum -checksum64 $checksum
+Get-ChocolateyWebFile $packageName $unpackFile -Url64bit $url64 -checksumtype $checksumtype -checksum64 $checksum
 Get-ChocolateyUnzip $unpackFile $unpackDir
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $setupFile
-Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $setupFile2
 Remove-Item $unpackDir -Recurse -Force
 Remove-Item $unpackFile
