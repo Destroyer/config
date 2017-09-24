5de004ebe91d07b74392628793b2aa66ea4e4822
Destroyer <vlachmir@fit.cvut.cz>
2017-09-24 23:00:46 +0200

    Update Asmedia_USB3_chocolateyInstall.ps1

--- windows/Asmedia_USB3_chocolateyInstall.ps1
+++ windows/Asmedia_USB3_chocolateyInstall.ps1
@@ -11,7 +11,8 @@ $setupFile = Join-Path "$unpackDir" "AsusSetup.exe"
 $checksumtype = 'sha256'
 $checksum64 ='67358f14e0a1c6483aaf04f826cde0c0587ee47d1d90b4fa72b37be3bb928ac8'
 $url64 = "http://dlcdnet.asus.com/pub/ASUS/misc/usb30/Asmedia_USB3_V116351.zip"
- 
+
+
 Get-ChocolateyWebFile $packageName $unpackFile -Url64bit $url64 -checksumtype $checksumtype -checksum64 $checksum
 Get-ChocolateyUnzip $unpackFile $unpackDir
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $setupFile
