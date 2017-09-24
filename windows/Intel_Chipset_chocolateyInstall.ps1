eed293976d0917503058bf7fed3892eaebc7b7e3
Destroyer <vlachmir@fit.cvut.cz>
2017-09-24 22:55:35 +0200

    Update Intel_Chipset_chocolateyInstall.ps1

--- windows/Intel_Chipset_chocolateyInstall.ps1
+++ windows/Intel_Chipset_chocolateyInstall.ps1
@@ -1,22 +1,19 @@
-# Intel_chipset_10.1.1.38
+# Intel_chipset_10.1.1.44
 # https://www.asus.com/Motherboards/Z170-A/HelpDesk_Download/
 
 $packageName = 'Intel_Chipset'
-$version = '10.1.1.38'
+$version = '10.1.1.44'
 $fileType = 'exe'
 $silentArgs = '-s'
 $unpackDir = New-Item "${ENV:TEMP}\intelchipsetdriver" -ItemType Directory -Force
-$unpackFile = "${ENV:TEMP}\Intel_chipset_101138.zip"
+$unpackFile = "${ENV:TEMP}\Chipset10.1.1.44_RS2_20170718.zip"
 $setupFile = Join-Path "$unpackDir" "SetupChipset.exe"
 $checksumtype = 'sha256'
-$checksum ='f7dd5b34eb18ebc5c690b05a163cc9f966b04c6c1ab6c24454f01bbec8f25252'
-$checksum64 ='f7dd5b34eb18ebc5c690b05a163cc9f966b04c6c1ab6c24454f01bbec8f25252'
-
-$url = "http://dlcdnet.asus.com/pub/ASUS/misc/utils/Intel_chipset_101138.zip"
-$url64 = "$url"
+$checksum64 ='ba6cbc7f22671354b3deebedf83282ee6a5bd8087e2a0cb20d57366d931821d5'
+$url64 = "http://dlcdnet.asus.com/pub/ASUS/mb/03CHIPSET/Chipset10.1.1.44_RS2_20170718.zip"
 
  
-Get-ChocolateyWebFile $packageName $unpackFile $url $url64 -checksumtype $checksumtype -checksum $checksum -checksum64 $checksum
+Get-ChocolateyWebFile $packageName $unpackFile -Url64bit $url64 -checksumtype $checksumtype -checksum64 $checksum
 Get-ChocolateyUnzip $unpackFile $unpackDir
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $setupFile
 Remove-Item $unpackDir -Recurse -Force
