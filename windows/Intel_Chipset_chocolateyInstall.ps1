7b2cc0bdf427584f368bc4c43032c0986468c473
Destroyer <vlachmir@fit.cvut.cz>
2018-04-01 22:49:24 +0200

    Update chipset driver

--- windows/Intel_Chipset_chocolateyInstall.ps1
+++ windows/Intel_Chipset_chocolateyInstall.ps1
@@ -1,19 +1,19 @@
-# Intel_chipset_10.1.1.44
+# Intel_chipset_11.7.0.1040
 # https://www.asus.com/Motherboards/Z170-A/HelpDesk_Download/
 
 $packageName = 'Intel_Chipset'
-$version = '10.1.1.44'
+$version = '11.7.0.1040'
 $fileType = 'exe'
 $silentArgs = '-s'
 $unpackDir = New-Item "${ENV:TEMP}\intelchipsetdriver" -ItemType Directory -Force
-$unpackFile = "${ENV:TEMP}\Chipset10.1.1.44_RS2_20170718.zip"
+$unpackFile = "${ENV:TEMP}\Consumer_11.7.0.1040.zip"
 $setupFile = Join-Path "$unpackDir" "AsusSetup.exe"
 $checksumtype = 'sha256'
-$checksum64 ='ba6cbc7f22671354b3deebedf83282ee6a5bd8087e2a0cb20d57366d931821d5'
-$url64 = "http://dlcdnet.asus.com/pub/ASUS/mb/03CHIPSET/Chipset10.1.1.44_RS2_20170718.zip"
+$checksum64 ='78638ddb6c4358813528e1d7bddfe93c7d02000a95bcc8c984b0f6af4dc09cc6'
+$url64 = "http://dlcdnet.asus.com/pub/ASUS/mb/03CHIPSET/Consumer_11.7.0.1040.zip"
 
  
-Get-ChocolateyWebFile $packageName $unpackFile -Url64bit $url64 -checksumtype $checksumtype -checksum64 $checksum
+Get-ChocolateyWebFile $packageName $unpackFile -Url64bit $url64 -checksumtype $checksumtype -checksum64 $checksum64
 Get-ChocolateyUnzip $unpackFile $unpackDir
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $setupFile
 Remove-Item $unpackDir -Recurse -Force
